d2617daf31c5454ebd6345d68b9ff9696d3bc02c
Luis Carlos Ballesteros <ballesteroshurtado@gmail.com>
2015-07-26 22:24:07 +0200

    Añadida lista de preguntas

--- models/models.js
+++ models/models.js
@@ -32,8 +32,8 @@ exports.Quiz = Quiz;
 sequelize.sync().success(function() {
 	Quiz.count().success(function(count){
 		if(count===0){
-			Quiz.create({pregunta:'Capital de Italia', respuesta:'Roma'}).success(function(){
-				console.log('base de Datos inicializada')});
+      Quiz.create({pregunta:'Capital de Italia', respuesta:'Roma'});
+			Quiz.create({pregunta:'Capital de Portugal', respuesta:'Lisboa'});
 		};
 	});
 });
