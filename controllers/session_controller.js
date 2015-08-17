69c09841820599d016999e95163029b7750dbee0
Luis Carlos Ballesteros <ballesteroshurtado@gmail.com>
2015-08-17 19:33:48 +0200

    Añadida autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,4 +1,10 @@
-
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
 
 // Get /login   -- Formulario de login
 exports.new = function(req, res) {
